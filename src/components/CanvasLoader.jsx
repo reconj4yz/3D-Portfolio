91d1c1f13f3cdf0833f06a7edcc0d466d0da6aaa
reconjayz <jaichawla263@gmail.com>
2024-09-21 22:41:26 +0530

    congiguring 3d models

---
+++ src/components/CanvasLoader.jsx
@@ -0,0 +1,33 @@
+import { Html, useProgress } from "@react-three/drei";
+
+const CanvasLoader = () => {
+  const { progress } = useProgress();
+
+  return (
+    <Html
+      as="div"
+      center
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <span className="canvas-loader" />
+      <p
+        style={{
+          fontSize: "14px",
+          color: "#F1F1F1",
+          fontWeight: "800",
+          marginTop: "40px",
+          textAlign: "center",
+        }}
+      >
+        {progress !== 0 ? `${progress.toFixed(2)}%` : "Loading..."}
+      </p>
+    </Html>
+  );
+};
+
+export default CanvasLoader;
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,3 +1,8 @@
+import { Canvas } from "@react-three/fiber";
+import { PerspectiveCamera } from "@react-three/drei";
+import HackerRoom from "../components/HackerRoom";
+import { Suspense } from "react";
+import CanvasLoader from "../components/CanvasLoader";
 const Hero = () => {
   return (
     <section className="min-h-screen w-full flex flex-col relative ">
@@ -5,10 +10,19 @@ const Hero = () => {
         <p className="sm:text-3xl text-2xl font-medium text-center text-white font-generalsans">
           Hi, I am Jai <span className="waving-hand">👋</span>
         </p>
-        <p className="hero_tag text-gray_gradient">Building products that shape the future.</p>
+        <p className="hero_tag text-gray_gradient">
+          Building products that shape the future.
+        </p>
       </div>
       <div className="w-full h-full absolute inset-0">
-        
+        <Canvas className="w-full h-full">
+          <Suspense fallback={<CanvasLoader />}>
+            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+            <HackerRoom scale={0.12} position={[0,-10,-1]} rotation={[0, 3.14, 0]}/>
+            <ambientLight intensity={1} />
+            <directionalLight position={[10,10,10]} intensity={0.5} />
+          </Suspense>
+        </Canvas>
       </div>
     </section>
   );
