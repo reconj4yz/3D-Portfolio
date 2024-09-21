58e2023894e0b69c34d38b32b10eacc52b9f64c1
reconjayz <jaichawla263@gmail.com>
2024-09-21 18:49:40 +0530

    initial commit before adding 3d models

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,17 @@
+const Hero = () => {
+  return (
+    <section className="min-h-screen w-full flex flex-col relative ">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="sm:text-3xl text-2xl font-medium text-center text-white font-generalsans">
+          Hi, I am Jai <span className="waving-hand">👋</span>
+        </p>
+        <p className="hero_tag text-gray_gradient">Building products that shape the future.</p>
+      </div>
+      <div className="w-full h-full absolute inset-0">
+        
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
