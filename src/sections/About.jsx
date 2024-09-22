f8cf6d81a02a815ed37108cf61d93e3e14eebf8f
reconjayz <jaichawla263@gmail.com>
2024-09-23 01:10:37 +0530

    about section complete

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,48 +1,106 @@
-import Globe from "react-globe.gl";
+import { useState } from 'react';
+import Globe from 'react-globe.gl';
+
+import Button from '../components/Button.jsx';
 
 const About = () => {
+  const [hasCopied, setHasCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText('jaichawla263@gmail.com');
+    setHasCopied(true);
+
+    setTimeout(() => {
+      setHasCopied(false);
+    }, 2000);
+  };
+
   return (
-    <section className="c-space my-20">
+    <section className="c-space my-20" id="about">
       <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
-        <div className="col-span-1 xl:row-span-4 ">
+        <div className="col-span-1 xl:row-span-3">
           <div className="grid-container">
-            <img
-              src="/assets/grid1.png"
-              alt="grid-1"
-              className="w-full sm:h-[276px] h-fit object-contain"
-            />
-          <div>
-            <p className="grid-headtext">Hi, I'm Jai Chawla</p>
-            <p className="grid-subtext">
+            <img src="assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
+
+            <div>
+              <p className="grid-headtext">Hi, I’m Jai Chawla</p>
+              <p className="grid-subtext">
               I'm a full-stack developer, passionate about creating innovative
-              solutions. I have a background in computer science and a strong
+              solutions. I have a strong
               foundation in web development.
-            </p>
-          </div>
+              </p>
+            </div>
           </div>
         </div>
-        <div className="col-span-1 xl:col-span-2 xl:row-span-4">
-            <div className="grid-container">
-                <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
-                    <Globe 
-                    height={326}
-                    width={752}
-                    backgroundColor="rgba(0,0,0,0)"
-                    backgroundImageOpacity={0.5}
-                    showAtmosphere={true}
-                    showGraticules={true}
-                    globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-                    bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
-                    labelsData={[{
-                        lat:28.636103, lng:77.097004,
-                        text:"I'm Here !",
-                        color: "#fff",
-                        font: "Arial",
-                        size: 35
-                    }]}
-                    />
-                </div>
+
+        <div className="col-span-1 xl:row-span-3">
+          <div className="grid-container">
+            <img src="assets/grid2.png" alt="grid-2" className="w-full sm:h-[276px] h-fit object-contain" />
+
+            <div>
+              <p className="grid-headtext">Tech Stack</p>
+              <p className="grid-subtext">
+              I specialize in JavaScript/TypeScript with a focus on React and Next.js ecosystems.
+              </p>
             </div>
+          </div>
+        </div>
+
+        <div className="col-span-1 xl:row-span-4">
+          <div className="grid-container">
+            <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
+              <Globe
+                height={326}
+                width={326}
+                backgroundColor="rgba(0, 0, 0, 0)"
+                backgroundImageOpacity={0.5}
+                showAtmosphere
+                showGraticules
+                globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+                labelsData={[{ lat: 40, lng: -100, text: 'Rjieka, Croatia', color: 'white', size: 15 }]}
+              />
+            </div>
+            <div>
+              <p className="grid-headtext">Ready to work remotely across various timezones.</p>
+              <p className="grid-subtext"> I'm based in New Delhi, Indi ready to work to build the website of your dreams remotely.</p>
+              <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
+            </div>
+          </div>
+        </div>
+
+        <div className="xl:col-span-2 xl:row-span-3">
+          <div className="grid-container">
+            <img src="assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain" />
+
+            <div>
+              <p className="grid-headtext">My Passion for Coding</p>
+              <p className="grid-subtext">
+                I love solving problems and building things through code. Programming isn&apos;t just my
+                profession—it&apos;s my passion. I enjoy exploring new technologies, and enhancing my skills.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="xl:col-span-1 xl:row-span-2">
+          <div className="grid-container">
+            <img
+              src="assets/grid4.png"
+              alt="grid-4"
+              className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"
+            />
+
+            <div className="space-y-2">
+              <p className="grid-subtext text-center">Contact me</p>
+              <div className="copy-container" onClick={handleCopy}>
+                <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'} alt="copy" />
+                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">
+                    jaichawla263@gmail.com
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
