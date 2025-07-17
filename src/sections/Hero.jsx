9e305655583519d8550fabc46d809882956fecb6
reconjayz <jaichawla263@gmail.com>
2025-07-17 18:20:18 +0530

    handling peroformance issues

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,16 +1,7 @@
-import { Canvas } from "@react-three/fiber";
-import { PerspectiveCamera } from "@react-three/drei";
-import HackerRoom from "../components/HackerRoom";
-import { Suspense } from "react";
-import CanvasLoader from "../components/CanvasLoader";
-// import { Leva, useControls } from "leva";
+
 import { useMediaQuery } from "react-responsive";
 import { calculateSizes } from "../constants";
-import Target from "../components/Target";
-import ReactLogo from "../components/ReactLogo";
-import Cube from "../components/Cube";
-import Rings from "../components/Ring";
-import HeroCamera from "../components/HeroCamera";
+
 import Button from "../components/Button";
 const Hero = () => {
   // const x = useControls("HackerRoom",{
@@ -55,8 +46,8 @@ const Hero = () => {
   const isTablet = useMediaQuery({minWidth:768 , maxWidth:1024})
   const sizes = calculateSizes(isSmall, isMobile, isTablet)
   return (
-    <section className="min-h-screen w-full flex flex-col relative " id="home">
-      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+    <section className="w-full flex flex-col relative " id="home">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space">
         <p className="sm:text-3xl text-2xl font-medium text-center text-white font-generalsans">
           Hi, I am Jai <span className="waving-hand">👋</span>
         </p>
@@ -64,31 +55,8 @@ const Hero = () => {
           Building products & Brands.
         </p>
       </div>
-      <div className="w-full h-full absolute inset-0">
-            {/* <Leva /> */}
-        <Canvas className="w-full h-full">
-          <Suspense fallback={<CanvasLoader />}>
-            <PerspectiveCamera makeDefault position={[0, 0, 20]} />
-            <HeroCamera>
-
-            <HackerRoom
-              scale={sizes.deskScale}
-              position={sizes.deskPosition}
-              rotation={[0,-Math.PI, 0]}
-              />
-            </HeroCamera>
-            <group>
-              <Target position={sizes.targetPosition}/>
-              <ReactLogo position={sizes.reactLogoPosition}/>
-              <Cube position={sizes.cubePosition} />
-              <Rings position={sizes.ringPosition} />
-            </group>
-            <ambientLight intensity={1} />
-            <directionalLight position={[10, 10, 10]} intensity={0.5} />
-          </Suspense>
-        </Canvas>
-      </div>
-      <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
+    
+      <div className="absolute bottom-[-4rem] left-0 right-0 w-full z-10 c-space">
         <a href="#about" className="w-fit">
           <Button name="Let's work together" isBeam containerClass="sm:w-fit w-full sm:min-w-96" />
         </a>
